f682edf68c330ce05cce78dbbe0582d19b0bb20b
William Voigt <wvoigt@Williams-Air.lan1>
2023-02-08 19:12:51 -0800

    added login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-// First we import our Hello component from our components folder.
-import Hello from './components/Hello';
+import React from "react";
+import Login from "./pages/login";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // React apps typically have a single App component at the very top that can reference other React components.
 // This component, `App`, is our main component that is importing `Hello` and rendering it in the return method.
 function App() {
-  return <Hello />;
+  return <Login />;
 }
 
 export default App;
